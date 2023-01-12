153ae2864f8769c9954acc67138c73d4f6717c56
Jamie Perkins <jamie.perkins@gmail.com>
2023-01-12 11:10:29 -0700

    layout

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const path = require('path')
+
 module.exports = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [
+      path.join(__dirname, 'styles'),
+      path.join(__dirname, 'components')
+    ],
+  },
 }
