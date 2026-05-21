d26d04af8d881b1efa0917b1849aa4614fa28044
inorganik <jamie.perkins@gmail.com>
2026-05-21 10:04:06 -0600

    link back to post

--- pages/jeffco-acfr-report/index.js
+++ pages/jeffco-acfr-report/index.js
@@ -49,8 +49,12 @@ export default function JeffcoAcfrReport() {
               $68 million budget shortfall
             </a>{' '}
             and as a dad of 2 JeffCo students, I wanted to understand the budget better. So I
-            compiled 20 years of data from JeffCo's Annual Comprehensive Financial Reports. Here are
-            the headlines:
+            compiled 20 years of data from JeffCo's Annual Comprehensive Financial Reports. I summed
+            up my thoughts in a{' '}
+            <a href="/posts/2026-05-21-jeffco-acfr-data" target="_blank">
+              blog post
+            </a>
+            , but here are the headlines:
           </p>
           <ul className="text-xl">
             <li>
